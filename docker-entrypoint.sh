abca47d2085ce291b71d8eac20338024d65cd31a
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-11-04 16:16:22 +0100

    add POD_IP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-CONTAINER_IP_ADDR=$(hostname -i)
+# Read first $POD_IP if not set get from hostname -i ip address
+export CONTAINER_IP_ADDR=${POD_IP:-$(hostname -i)}
 echo "Container local ip addr is $CONTAINER_IP_ADDR"
-export CONTAINER_IP_ADDR
 
-# only one process to run
-# do not need supervisord
-# start command
+# there is only one process to run
+# we do not need to use supervisord
+# start command node file-service.js
 cd /composer/node/file-service
 node file-service.js
-
